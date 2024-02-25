769e53b46c97e4a0d5392e9a32db3bb65d095929
hxgdzyuyi <hxgdzyuyi@gmail.com>
2024-02-25 15:31:45 +0800

    feat: add user slice

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -5,6 +5,8 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const queryClient = new QueryClient();
 
@@ -21,9 +23,11 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </QueryClientProvider>,
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </QueryClientProvider>
+  </Provider>,
 );
